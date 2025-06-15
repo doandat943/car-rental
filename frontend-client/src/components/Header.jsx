5022029c051b9f3d78e72d22d380c89b33d0d882
doandat943 <doandat943@joverse.us>
2025-06-15 23:57:36 +0700

    frontend-client: Adjust header link hover effect

--- frontend-client/src/components/Header.jsx
+++ frontend-client/src/components/Header.jsx
@@ -116,7 +116,7 @@ export default function Header() {
                 className="relative px-4 py-2 font-semibold text-gray-700 transition-all duration-300 rounded-lg hover:text-blue-600 hover:bg-blue-50 group"
               >
                 <span className="relative z-10">{item.label}</span>
-                <div className="absolute bottom-0 left-1/2 w-0 h-0.5 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:w-3/4 group-hover:left-1/8 transition-all duration-300"></div>
+                <div className="absolute bottom-0 left-1/2 w-0 h-0.5 bg-gradient-to-r from-blue-500 to-purple-500 transition-all duration-300 -translate-x-1/2 group-hover:w-3/4"></div>
               </Link>
             ))}
           </nav>
